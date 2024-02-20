1e0ff4eb67e7146ef2fbfda5d3509758db8a7c3d
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2024-02-20 21:08:53 +0200

    [ADD]: Tabs

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,9 @@
-import React, { useState } from "react";
-import { Image } from "expo-image";
-import Animated, { useSharedValue } from "react-native-reanimated";
-import { View, Text, Pressable } from "react-native";
-import { AntDesign } from '@expo/vector-icons';
-import { Link } from "expo-router";
-import { Slot } from "expo-router";
-
-const imagePath = "../assets/writing.png";
+import { Stack } from "expo-router";
 
 export default function Main(){
 	return (
-		<Slot />
+		<Stack>
+			<Stack.Screen name="(notes)" />
+		</Stack>
 	)
 }
