f97bdcc299a2612400c6f73a9edb233fb4f55f6a
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2024-02-20 21:17:51 +0200

    [DELET]: Header

--- app/_layout.js
+++ app/_layout.js
@@ -3,7 +3,13 @@ import { Stack } from "expo-router";
 export default function Main(){
 	return (
 		<Stack>
-			<Stack.Screen name="(notes)" />
+			<Stack.Screen 
+			name="(notes)" 
+			options={{
+				// Hide the header
+	    		headerShown: false
+			}}
+			/>
 		</Stack>
 	)
 }
