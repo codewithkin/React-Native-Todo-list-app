d5ce04e8b42182e9b7d7f557195861245dca8193
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2024-02-20 22:08:56 +0200

    [ADD]: Icons to bottom tabs

--- app/(notes)/_layout.js
+++ app/(notes)/_layout.js
@@ -1,4 +1,6 @@
 import { Tabs } from "expo-router/tabs";
+import { Octicons } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
   return (
@@ -7,17 +9,34 @@ export default function App() {
 		name="index" 
 		options={{
 			tabBarLabel: "Dashboard",
-    		headerShown: false
+			tabBarLabelStyle: {
+				color: "#083D77",
+				fontWeight: "bold",
+				fontSize: 12
+			},
+			tabBarIcon: ({ color, size }) => (
+				<Octicons name="home" size={25} color="#083D77" />
+			),
+    		headerShown: false,
 		}}
 		/>
 
     	<Tabs.Screen 
-    	name="all" 
+    	name="all"
     	options={{
     		tabBarLabel: "All notes",
+    		tabBarLabelStyle: {
+				color: "#083D77",
+				fontWeight: "bold",
+				fontSize: 12
+			},
+			tabBarIcon: ({ color, size }) => (
+				<Entypo name="list" size={25} color="#083D77" />
+			),
     		headerShown: false
     	}}
     	/>
+
     </Tabs>
   );
 }
